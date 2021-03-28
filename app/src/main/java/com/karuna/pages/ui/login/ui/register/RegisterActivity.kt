1d645bbf66b0dcbcedaf971528458443e2f353e5
Johnstone Ananda <johnolwamba@gmail.com>
2021-03-28 15:23:33 -0500

    add validation to register flow

--- app/src/main/java/com/karuna/pages/ui/login/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/karuna/pages/ui/login/ui/register/RegisterActivity.kt
@@ -1,13 +1,71 @@
 package com.karuna.pages.ui.login.ui.register
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.karuna.pages.MainActivity
 import com.karuna.pages.R
+import com.karuna.pages.ui.BaseActivity
+import com.karuna.pages.utils.Resource
+import kotlinx.android.synthetic.main.activity_register.*
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
+
+    private lateinit var viewModel: RegisterViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
+        registerUiListeners()
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.uiState.observe(this, Observer {
+            when (it.status) {
+                Resource.Status.SUCCESS -> {
+                    Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+                    showLoadingIndicator(false)
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+                Resource.Status.ERROR -> {
+                    showLoadingIndicator(false)
+                    Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+                }
+
+                Resource.Status.LOADING -> {
+                    showLoadingIndicator(true)
+                    Toast.makeText(this, "Loading!!!!!!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
+
+    /**
+     * Register ui listeners
+     */
+    private fun registerUiListeners() {
+        btnRegister.setOnClickListener {
+            val name = nameTextView.text.toString()
+            val email = txtUsername.text.toString()
+            val password = txtPassword.text.toString()
+            if (email.isBlank()) {
+                Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show()
+            } else if (password.isBlank()) {
+                Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show()
+            } else {
+                registerUser(name,email, password)
+            }
+        }
+
+    }
+
+    private fun registerUser(name: String, email: String, password: String) {
+        showLoadingIndicator(true)
+        viewModel.registerUser(name, email, password)
     }
 }
 
--- app/src/main/java/com/karuna/pages/ui/login/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/karuna/pages/ui/login/ui/register/RegisterViewModel.kt
@@ -1,5 +1,22 @@
 package com.karuna.pages.ui.login.ui.register
 
-class RegisterViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.karuna.pages.data.entities.User
+import com.karuna.pages.data.repository.UserRepository
+import com.karuna.pages.utils.Resource
+import kotlinx.coroutines.launch
 
+class RegisterViewModel : ViewModel() {
+    private var repository = UserRepository()
+
+    val uiState: MutableLiveData<Resource<User>> = MutableLiveData()
+
+    fun registerUser(name: String, email: String, password: String) {
+        viewModelScope.launch {
+            //save user in local db here or in repository and set logged in to true
+            uiState.value = repository.registerUser(name, email, password)
+        }
+    }
 }
